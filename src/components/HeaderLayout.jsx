9120b5e860990a3a3f615690f0253b1cfaa28922
hoki9797 <hoki9797@gmail.com>
2025-02-25 13:00:46 +0900

    fix: 로그아웃 시 캐시 무효화

--- src/components/HeaderLayout.jsx
+++ src/components/HeaderLayout.jsx
@@ -1,8 +1,11 @@
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import ROUTER_URL from '../constants/routerURL';
 import useAuthStore from '../store/authStore';
+import { useQueryClient } from '@tanstack/react-query';
+import { queryKeys } from '../constants/queryKeys';
 
 function HeaderLayout() {
+  const queryClient = useQueryClient();
   const isAuthenticated = useAuthStore(state=>state.isAuthenticated)
   const clearAccessToken = useAuthStore(state=>state.clearAccessToken)
   const navigate = useNavigate();
@@ -10,7 +13,9 @@ function HeaderLayout() {
   // 토큰을 지우고 로그아웃
   const handleLogout = () => {
     alert('로그아웃 되었습니다!');
+    queryClient.invalidateQueries([queryKeys.USER_PROFILE])
     clearAccessToken();
+
     navigate(ROUTER_URL.SIGNIN);
   };
 
